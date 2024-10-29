e8657e568004a97921cc8a8e5d440c2c6044e6d1
Halva1988 <khalaimaleksandr@gmail.com>
2024-10-29 10:48:19 +0300

    init project, сщздал компоненты: header, footer; создал Link в header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+
+const Header = () => {
+  return (
+    <header>
+      <Link href="/">Home</Link>
+      <Link href="/events">All Events</Link>
+    </header>
+  )
+}
+
+export default Header
